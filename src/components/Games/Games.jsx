2162057f7d7972dac52eddf8b6c3296e0ed5531b
Yoshidda <70910101+Yoshidda@users.noreply.github.com>
2021-04-02 12:07:35 +0100

    perforname ehancement tweaks

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -1,53 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import CardList from '../CardList/CardList';
+import React, { useState, useEffect } from "react";
+import CardList from "../CardList/CardList";
 import SearchBar from "../SearchBar/SearchBar";
 import Header from "../Header/Header";
-import StaticCards from '../StaticCards/StaticCards';
-
-
+import StaticCards from "../StaticCards/StaticCards";
 
 const Games = () => {
+  const [games, setGames] = useState([]);
 
-    const [games, setGames] = useState([])
+  const [searchField, setSearchField] = useState("");
 
-    const [searchField, setSearchField] = useState("")
-
-
-    const fetchData = async () => {
-        const apiKey = "8e8b94ef98d6450087c462de886f297c"
-        try {
-
-            const response = await fetch(`https://api.rawg.io/api/games?search=${searchField}&key=${apiKey}&dates=2015-09-01,2021-01-01&platforms=18,1`)
-            const parsedResponse = await response.json()
-            const data = parsedResponse.results
-            console.log(data)
-            setGames(data)
-
-
-        } catch (error) {
-            console.log(error)
-        }
+  const fetchData = async () => {
+    const apiKey = "8e8b94ef98d6450087c462de886f297c";
+    try {
+      const response = await fetch(
+        `https://api.rawg.io/api/games?search=${searchField}&key=${apiKey}&dates=2015-09-01,2021-01-01&platforms=18,1`
+      );
+      const parsedResponse = await response.json();
+      const data = parsedResponse.results;
+      console.log(data);
+      setGames(data);
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
+  useEffect(
+    (e) => {
+      if (e.key === "Enter") {
+        fetchData();
+      }
+    },
+    [searchField]
+  );
 
-    useEffect(() => {
-        fetchData()
-        console.log(games)
-    }, [searchField])
-
-
-
-
-    return (
-        <div>
-            <Header />
-            <StaticCards />
-            <SearchBar setSearchField={setSearchField}
-                       searchField={searchField} />
-            <CardList games={games} />
-        </div>
-
-    )
-}
+  return (
+    <div>
+      <Header />
+      <StaticCards />
+      <SearchBar setSearchField={setSearchField} searchField={searchField} />
+      <CardList games={games} />
+    </div>
+  );
+};
 
 export default Games;
